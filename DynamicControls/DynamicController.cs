ea96221ad85e3fc6fa3596b472859bc51b64e912
Scogun <SAntonov@UCASoft.com>
2015-12-16 16:41:30 +0300

    Extract IDynamicController interface.

--- DynamicControls/DynamicController.cs
+++ DynamicControls/DynamicController.cs
@@ -8,8 +8,35 @@ namespace DynamicControls
     /// <summary>
     /// The dynamic controller.
     /// </summary>
-    public class DynamicController : Controller
+    public class DynamicController : Controller, IDynamicController
     {
+        /// <summary>
+        /// The get childs html.
+        /// </summary>
+        /// <param name="areaControl">
+        /// The area control.
+        /// </param>
+        /// <param name="parentName">
+        /// The parent name.
+        /// </param>
+        /// <param name="parentValue">
+        /// The parent value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="HtmlString"/>.
+        /// </returns>
+        public static HtmlString GetChildsHtml(JObject areaControl, string parentName, string parentValue)
+        {
+            if (areaControl != null)
+            {
+                JObject requestControl =
+                    areaControl.SelectToken(string.Format("$.childs..controls[?(@.name == '{0}')]", parentName)) as JObject;
+                IDynamicParentControl control = BaseControl.CreateControl(requestControl) as IDynamicParentControl;
+                if (control != null) return new HtmlString(control.RenderChilds(parentValue));
+            }
+            return null;
+        }
+
         /// <summary>
         /// The get childs.
         /// </summary>
@@ -28,14 +55,7 @@ namespace DynamicControls
         public HtmlString GetChilds(string areaName, string parentName, string parentValue)
         {
             JObject areaControl = Session[DynamicControlsBuilder.GetAreaTempDataKey(areaName)] as JObject;
-            if (areaControl != null)
-            {
-                JObject requestControl = areaControl.SelectToken(string.Format("$.childs..controls[?(@.name == '{0}')]", parentName)) as JObject;
-                IDynamicParentControl control = BaseControl.CreateControl(requestControl) as IDynamicParentControl;
-                if (control != null)
-                    return new HtmlString(control.RenderChilds(parentValue));
-            }
-            return null;
+            return GetChildsHtml(areaControl, parentName, parentValue);
         }
     }
 }
---
+++ DynamicControls/IDynamicController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+
+namespace DynamicControls
+{
+    /// <summary>
+    /// The DynamicController interface.
+    /// </summary>
+    public interface IDynamicController
+    {
+        /// <summary>
+        /// The get childs.
+        /// </summary>
+        /// <param name="areaName">
+        /// The area name.
+        /// </param>
+        /// <param name="parentName">
+        /// The parent name.
+        /// </param>
+        /// <param name="parentValue">
+        /// The parent value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="HtmlString"/>.
+        /// </returns>
+        HtmlString GetChilds(string areaName, string parentName, string parentValue);
+    }
+}
