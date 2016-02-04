971d5f5d9182dca027b6ac9eadef3741203ca563
Sergey Antonov <SAntonov@UCASoft.com>
2016-02-04 18:03:02 +0300

    A little work

---
+++ DynamicControls/Controls/IDynamicValueControl.cs
@@ -0,0 +1,16 @@
+﻿namespace DynamicControls.Controls
+{
+    /// <summary>
+    /// The DynamicValueControl interface.
+    /// </summary>
+    public interface IDynamicValueControl
+    {
+        /// <summary>
+        /// The set parent value.
+        /// </summary>
+        /// <param name="parentValue">
+        /// The parent value.
+        /// </param>
+        void SetParentValue(string parentValue);
+    }
+}
